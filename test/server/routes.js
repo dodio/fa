90da2e6664997cd5992efe46fe7e58107b10cd0c
dodio <go2solo18@gmail.com>
2016-04-17 04:24:10 +0800

    美化代码格式

--- test/server/routes.js
+++ test/server/routes.js
@@ -1,41 +1,41 @@
-module.exports = function (router) {
-    
-  router.get("/login", function (req, res) {
+module.exports = function(router) {
+
+  router.get("/login", function(req, res) {
     req.session.user = {
-        name: 'xiyang',
-        email: 'go2solo18@gmail.com'
+      name: 'xiyang',
+      email: 'go2solo18@gmail.com'
     }
     res.send("已经成功登陆，请返回");
   });
-  
+
   router.use(checkLogin);
   router.get("/", function(req, res, next) {
-      res.locals.doit = function(){
-          console.log('i want it');
-      }
-      res.cookie("testcookie", "testvalue",{
-        signed:false
-      });
-      res.cookie("testsign", "testvalue",{
-        signed:true
-      });
+    res.locals.doit = function() {
+      console.log('i want it');
+    }
+    res.cookie("testcookie", "testvalue", {
+      signed: false
+    });
+    res.cookie("testsign", "testvalue", {
+      signed: true
+    });
 
-      res.render("index",res.data.get())
+    res.render("index", res.data.get())
   })
 
-  router.get("/about", function (req, res, next){
-      res.render("page/about",{
-          title:"关于我们"
-      });
+  router.get("/about", function(req, res, next) {
+    res.render("page/about", {
+      title: "关于我们"
+    });
   })
 
-  router.post("/api/data", function (req, res){
+  router.post("/api/data", function(req, res) {
     res.json(req.body);
   })
-  
-  router.get("/api/tngou",function(req, res){
+
+  router.get("/api/tngou", function(req, res) {
     var tngou = fa.bee('tngou');
-    tngou.get('/info/list', function(err, data){
+    tngou.get('/info/list', function(err, data) {
       res.send(JSON.parse(data.body))
     })
   })
@@ -43,9 +43,9 @@ module.exports = function (router) {
 
 
 function checkLogin(req, res, next) {
-    if(req.session.user){
-        res.data('user',req.session.user);
-        return next();
-    }
-    res.redirect("/login");
+  if (req.session.user) {
+    res.data('user', req.session.user);
+    return next();
+  }
+  res.redirect("/login");
 }
