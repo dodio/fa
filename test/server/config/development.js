c6834756bb9cd156789a535e80087874d77efdc2
dodio <go2solo18@gmail.com>
2016-04-17 02:46:13 +0800

    bee api 配置与管理(基于node request)

--- test/server/config/development.js
+++ test/server/config/development.js
@@ -10,18 +10,39 @@ module.exports = {
   },
 
   "bee": {
-    // 数据api集群
-    "user": {
-      // 版本
-      "latest":{
-        "defaults":{
+    //global defaults to bee.
+    "defaults": {
+      "headers": {
+        "apikey": "d006b78fed6046dec232092e38335439"
+      }
+    },
+    // api集群,这里以百度apistore 中 天狗健康资讯为例：http://apistore.baidu.com/apiworks/servicedetail/888.html
+    "releases": {
+      "tngou": {
+        "defaults": {}, 
+        "latest": "v1",
 
-        },
-        "servers":[
-          {
+        "releases": {
+          "v1": {
+            "version": "1.0.0",
+            "defaults": {},
+            "latest": "20160416",
 
+            "releases": {
+              "20160416": {
+                "version": "20160416",
+                "defaults": {},
+                //servers 与 versions 必须有一个，servers 优先，如果配置了servers，子版本将被忽略, 也忽略 defaultVersion
+                "servers": [
+                  //就是单独的一个 request defaults 配置
+                  {
+                    "baseUrl": "http://apis.baidu.com/tngou/"
+                  }
+                ]
+              }
+            }
           }
-        ]
+        }
       }
     }
   }
